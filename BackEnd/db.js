ebcdbbbb1b4fcc54af54fd7cc12c94d024c3a2f7
Abdi3214 <raboy454>
2025-08-04 18:03:00 +0300

    update db.js

--- BackEnd/db.js
+++ BackEnd/db.js
@@ -1,10 +1,9 @@
 // db.js
 const mongoose = require('mongoose');
-
+require("dotenv").config();
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URL);
-
     console.log('✅ MongoDB connected');
   } catch (err) {
     console.error('❌ MongoDB connection failed:', err.message);
